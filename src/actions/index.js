51ce8ad85c84e174706a7a2a5997fc290d03a672
Alex Bondarenko <bond.alexx@gmail.com>
2023-04-08 16:14:43 +0300

    deleted actions folder

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import {productsFetching,productsFetched,productsFetchingError} from '../components/productList/prodcutsSlice'
-
-export const fetchProducts = (request) => (dispatch) => {
-    dispatch(productsFetching());
-    request("https://fakestoreapi.com/products")
-        .then(data => dispatch(productsFetched(data)))
-        .catch(() => dispatch(productsFetchingError()))
-}
-
